ec6635ea7862300cc739ae19e0adde180a9f6ee5
Akinlua Bolamigbe <41027615+alob-mtc@users.noreply.github.com>
2023-05-01 11:22:21 +0000

    clean up fuion signature

--- eventloop.go
+++ eventloop.go
@@ -13,41 +13,38 @@ var (
 	GlobalEventLoop *eventLoop
 )
 
-func Init() {
-	queue := []func() bool{}
-	var lock sync.Mutex
+func InitGlobalEventLoop() Future {
 	once.Do(func() {
+		queue := []func() bool{}
+		var lock sync.Mutex
+		// event loop
+		go func() {
+			for work := range eventBus {
+				lock.Lock()
+				queue = append(queue, *work)
+				lock.Unlock()
+			}
+		}()
+
+		go func() {
+			for {
+				n := len(queue)
+				retry := []func() bool{}
+				for i := 0; i < n; i++ {
+					currentWork := queue[i]
+
+					if done := currentWork(); !done {
+						retry = append(retry, currentWork)
+					}
+				}
+				lock.Lock()
+				queue = queue[n:]
+				queue = append(queue, retry...)
+				lock.Unlock()
+			}
+		}()
 		GlobalEventLoop = &eventLoop{promiseQueue: make([]*Promise, 0)}
 	})
-	// TODO: add evnt loop
-	go func() {
-		for work := range eventBus {
-			lock.Lock()
-			queue = append(queue, *work)
-			lock.Unlock()
-		}
-	}()
-
-	go func() {
-		for {
-			n := len(queue)
-			retry := []func() bool{}
-			for i := 0; i < n; i++ {
-				currentWork := queue[i]
-
-				if done := currentWork(); !done {
-					retry = append(retry, currentWork)
-				}
-			}
-			lock.Lock()
-			queue = queue[n:]
-			queue = append(queue, retry...)
-			lock.Unlock()
-		}
-	}()
-}
-
-func GetGlobalEventLoop() Future {
 	return GlobalEventLoop
 }
 
